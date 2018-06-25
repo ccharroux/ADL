2eac911dcf618e1cbcc8ec189c20bdd3d39afb1e
Brian Jenkins <bjenkins@millerkaplan.com>
2018-06-25 11:06:00 -0700

    DEV-3120 updated the market size list page to remove un-needed code and edited some text. Added market sizes to the settings menu.

--- InternalWebApp/InternalWebApp/js/main.js
+++ InternalWebApp/InternalWebApp/js/main.js
@@ -454,6 +454,7 @@ function buildMainMenu(selectedItem) {
     menuItems += '        <li class="dropdown"><a ' + getSelectedItemClass(selectedItem, "Settings") + ' role="button" aria-expanded="false">Settings <span style="margin-right:10px;" class="caret"></span></a>';
     menuItems += '              <ul class="dropdown-menu" role="menu">';
     menuItems += '                  <li style="display:block;"><a href="formatlist.html">Formats</a></li>';
+    menuItems += '                  <li style="display:block;"><a href="marketsizelist.html">Market Sizes</a></li>';
     menuItems += '                  <li style="display:block;"><a href="positionlist.html">Positions</a></li>';
     menuItems += '                  <li style="display:block;"><a href="regionlist.html">Regions</a></li>';
     menuItems += '                  <li style="display:block;"><a href="revenuecategorieslist.html">Revenue Categories</a></li>';
