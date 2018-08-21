084b3831ee5f8a2cbfecdf0e8675149f8ddf4ebc
Brian Jenkins <bjenkins@millerkaplan.com>
2018-08-21 13:42:58 -0700

    DEV-3477 initial checkin of the generic search page. DEV-3144 additional work on the station advertiser page. Building both pages together to create the search functionality.

---
+++ InternalWebApp/InternalWebApp/js/search.js
@@ -0,0 +1,14 @@
+﻿function getSearchData(searchType) {
+    //get session object
+    //switch statement on token
+    //make api call can return the data
+    //turn into json that the datatable can understand
+    switch(searchType.toUpperCase()) {
+        case "":
+            break;
+        default:
+    }
+
+    //make api call and return data
+
+}
