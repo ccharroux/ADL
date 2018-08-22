50197938a31ac682a3f97baf24eec73804f5f3cc
Brian Jenkins <bjenkins@millerkaplan.com>
2018-08-22 15:43:56 -0700

    DEV-3477 continued work on the generic search page. Did add functionality to hide columns and only use specific columns from the data coming back from the API.

--- InternalWebApp/InternalWebApp/js/search.js
+++ InternalWebApp/InternalWebApp/js/search.js
@@ -6,22 +6,27 @@
     var api = "";
     var apiParameters="";
     var apiToken = getLocalStorage("APIToken");
-
+    var keepColumns = [];
+    var hideColumns = [];
 
     switch (searchCriteria["searchToken"].toUpperCase()) {
         case "STATIONMARKET":
-            console.log("search js: " + searchCriteria["searchToken"]);
-            //inApiToken
-            //inMarketId
-            //inAdvertiserName
-            //inShowDisabled
+//            console.log("search js: " + searchCriteria["searchToken"]);
             apiParameters = {
                 "inApiToken": apiToken,
                 "inMarketId": searchCriteria["marketID"],
-                "inAdvertiserName": searchCriteria["advertisterName"],
+                "inAdvertiserName": searchCriteria["advertiserName"],
                 "inShowDisabled": false
             }
             api = "/api/Advertiser/GetAdvertiserList";
+           
+            hideColumns.push("advertiserId");
+            hideColumns.push("marketId");
+
+            keepColumns.push("advertiserId");
+            keepColumns.push("advertiserName");
+            keepColumns.push("marketId");
+            keepColumns.push("marketName");
             break;
         default:
     }
@@ -36,7 +41,7 @@
         processData: false,
         success: function (data, textStatus, jQxhr) {
             if (data.response.status.toUpperCase() === "SUCCESS") {
-                loadSearchResults(data);
+                loadSearchResults(data,keepColumns,hideColumns);
             }
         },
         error: function (jqXhr, textStatus, errorThrown) {
@@ -45,43 +50,49 @@
     });
 }
 
-function loadSearchResults(data) {
-    console.log(data);
-
-    //build columns json
-    //var cols = [
-    //{ "mDataProp": "Field1", sTitle: "Date", sType : "date"},
-    //{ "mDataProp": "Field2", sTitle: "Number", sType : "numeric"},
-    //{ "mDataProp": "Field3" , "sTitle": "FName", sType : "string"},
-    //{ "mDataProp": "Field4" ,  sTitle: "LName", sType : "string"}
-    //];
-    //data.report.rows
+function loadSearchResults(data, keepColumns, hideColumns) {
 
     var results = data["report"]["rows"];
-    //console.log(results);
-
+    
     var cols = [];
+
+    //console.log(keepColumns);
 
     if (results.length > 0) {
         var columnsIn = results[0];
         for (var key in columnsIn) {
-            //console.log(key); // here is your column name you are looking for
-            var col = new tableColumn(key);
-            cols.push(col);
+
+            if (keepColumns.indexOf(key) !== -1) {
+                var visible = false;
+
+                if (hideColumns.indexOf(key) !== -1) {
+                    visible = false;
+                } else {
+                    visible = true;
+                }
+                
+                var col = new tableColumn(key, visible);
+                cols.push(col);
+            }
         }
     }
 
-   // var colJson = JSON.stringify(cols);
+    //var colJson = JSON.stringify(cols);
 
-    var searchTable = $("#dtSearchResults").dataTable({
+    var searchTable = $("#dtSearchResults").DataTable({
         "aaData": results,
-        "aoColumns": cols
+        "aoColumns": cols,
+        language: {
+            //this changes search box label from Search: to Filter Results:
+            search: "Filter Results:"
+        }
     });
 
 
 }
 
-function tableColumn(fieldTitle) {
+function tableColumn(fieldTitle, visible) {
     this.mDataProp = fieldTitle;
     this.sTitle = fieldTitle;
+    this.bVisible = visible;
 }
