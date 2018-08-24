1cb3dba2668971f704fe644d8d8323c39681d88f
Brian Jenkins <bjenkins@millerkaplan.com>
2018-08-24 16:17:21 -0700

    DEV-3144 worked on the station advertiser page. DEV-3477 continued work on the generic search page.

--- InternalWebApp/InternalWebApp/js/search.js
+++ InternalWebApp/InternalWebApp/js/search.js
@@ -4,22 +4,30 @@
     //make api call can return the data
     //turn into json that the datatable can understand
     var api = "";
-    var apiParameters="";
+    var apiParameters = "";
     var apiToken = getLocalStorage("APIToken");
     var keepColumns = [];
     var hideColumns = [];
 
+    //console.log(searchCriteria["searchToken"]);
+
     switch (searchCriteria["searchToken"].toUpperCase()) {
         case "STATIONMARKET":
-//            console.log("search js: " + searchCriteria["searchToken"]);
+            var searchText = "";
+            if ($('#txtSearchString').val().length > 0) {
+                searchText = $('#txtSearchString').val();
+            } else {
+                searchText = searchCriteria["marketAdvertiserName"].length > 0 ? searchCriteria["marketAdvertiserName"] : searchCriteria["advertiserName"];
+            }
+
             apiParameters = {
                 "inApiToken": apiToken,
                 "inMarketId": searchCriteria["marketID"],
-                "inAdvertiserName": searchCriteria["advertiserName"],
+                "inAdvertiserName": searchText,
                 "inShowDisabled": false
             }
             api = "/api/Advertiser/GetAdvertiserList";
-           
+
             hideColumns.push("advertiserId");
             hideColumns.push("marketId");
 
@@ -41,7 +49,7 @@
         processData: false,
         success: function (data, textStatus, jQxhr) {
             if (data.response.status.toUpperCase() === "SUCCESS") {
-                loadSearchResults(data,keepColumns,hideColumns);
+                loadSearchResults(data, keepColumns, hideColumns);
             }
         },
         error: function (jqXhr, textStatus, errorThrown) {
@@ -53,10 +61,7 @@
 function loadSearchResults(data, keepColumns, hideColumns) {
 
     var results = data["report"]["rows"];
-    
     var cols = [];
-
-    //console.log(keepColumns);
 
     if (results.length > 0) {
         var columnsIn = results[0];
@@ -70,20 +75,37 @@ function loadSearchResults(data, keepColumns, hideColumns) {
                 } else {
                     visible = true;
                 }
-                
+
                 var col = new tableColumn(key, visible);
                 cols.push(col);
             }
         }
+    } else {
+        for (var i = 0; i < keepColumns.length; i++) {
+            var visible = false;
+
+            if (hideColumns.indexOf(keepColumns[i]) !== -1) {
+                visible = false;
+            } else {
+                visible = true;
+            }
+
+            var col = new tableColumn(keepColumns[i], visible);
+            cols.push(col);
+        }
     }
 
-    //var colJson = JSON.stringify(cols);
+    var searchTable;
 
-    var searchTable = $("#dtSearchResults").DataTable({
+    if ($.fn.DataTable.isDataTable('#dtSearchResults')) {
+        searchTable = $('#dtSearchResults').DataTable();
+        searchTable.destroy();
+    }
+
+    searchTable = $("#dtSearchResults").DataTable({
         "aaData": results,
         "aoColumns": cols,
         language: {
-            //this changes search box label from Search: to Filter Results:
             search: "Filter Results:"
         }
     });
@@ -95,4 +117,58 @@ function tableColumn(fieldTitle, visible) {
     this.mDataProp = fieldTitle;
     this.sTitle = fieldTitle;
     this.bVisible = visible;
+}
+
+function cancelSearch(searchCriteria) {
+
+    var searchResults = {};
+
+    for (var key in searchCriteria) {
+        searchResults[key] = searchCriteria[key];
+    }
+
+    setLocalStorage("gSearchResults", JSON.stringify(searchResults));
+
+    var searchPage = searchCriteria["searchPage"]["href"];
+    window.location = searchPage;
+
+}
+
+function linkAdvertiser(searchCriteria) {
+    //will need to load local storage
+    //will probably implement history -1
+    //check if line is selected if not error
+
+    var searchTable = $('#dtSearchResults').DataTable();
+    if (searchTable.rows('.selected').any() === false) {
+        bootbox.alert('Select an advertiser to link.', function () {
+        });
+        return;
+    }
+
+    var rowData = searchTable.rows('.selected').data();
+    var searchResults = {};
+
+    for (var key in searchCriteria) {
+        searchResults[key] = searchCriteria[key];
+    }
+
+    //console.log(searchResults);
+    //console.log(rowData);
+
+    searchResults['marketAdvertiserId'] = rowData[0].advertiserId;
+    searchResults['marketAdvertiserName'] = rowData[0].advertiserName;
+
+    //console.log(searchResults);
+
+    setLocalStorage("gSearchResults", JSON.stringify(searchResults));
+
+    var searchPage = searchCriteria["searchPage"]["href"];
+    window.location = searchPage;
+
+}
+
+function fixAdvertiser() {
+    bootbox.alert('This functionality is still under development.', function () {
+    });
 }
