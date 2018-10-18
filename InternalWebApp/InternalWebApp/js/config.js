368ff973e3434a492ed0b9f494a2b755f78aec8c
MKACPA\ccharroux <ccharroux@millerkaplan.com>
2018-10-18 14:42:15 -0700

    added staging publish and config.js settings updates

--- InternalWebApp/InternalWebApp/js/config.js
+++ InternalWebApp/InternalWebApp/js/config.js
@@ -2,7 +2,15 @@ var gTimeToExpire = 6000; // seconds
 var ServicePrefix = "http://devservices.millerkaplan.com";
 
 
-if (window.location.toString().toLowerCase().indexOf("dev") > -1) {
+if (window.location.toString().toLowerCase().indexOf("devmediainternal.millerkaplan.com") > -1) {
     ServicePrefix = "https://devservices.millerkaplan.com/";
 }
+else if (window.location.toString().toLowerCase().indexOf("stagingmediainternal.millerkaplan.com") > -1) {
+    ServicePrefix = "https://stagingservices.millerkaplan.com/";
+}
+else if (window.location.toString().toLowerCase().indexOf("mediainternal.millerkaplan.com") > -1) {
+    ServicePrefix = "https://services.millerkaplan.com/";
+}
+
+
 
