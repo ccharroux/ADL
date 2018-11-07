57cd6ea388923b8fdd8f414cd97fa7ab09edebdf
MKACPA\ccharroux <ccharroux@millerkaplan.com>
2018-11-07 08:36:20 -0800

    generic reporting changes

--- InternalWebApp/InternalWebApp/js/genericReport.js
+++ InternalWebApp/InternalWebApp/js/genericReport.js
@@ -1,4 +1,7 @@
 const rptMRRMarketSummary = 1
+const rptXRAYUsage = 2
+const rptMRRUsage = 3
+const rptTVBUsage = 4
 
 function getReportObject(inReportId)
 {
@@ -10,46 +13,56 @@ function getReportObject(inReportId)
         arrayObject = getReportObject_MRRMarketSummary();
     }
 
+    if (inReportId == rptXRAYUsage)
+    {
+        arrayObject = getReportObject_XRAYUsage();
+
+    }
+
+    if (inReportId == rptMRRUsage) {
+        arrayObject = getReportObject_MRRUsage();
+    }
+
+    if (inReportId == rptTVBUsage) {
+        arrayObject = getReportObject_TVBUsage();
+    }
+
     return arrayObject;
 }
 function getReportFilterArray(inReportId) {
 
     var arrayFilters = new Array();
- 
 
     if (inReportId == rptMRRMarketSummary) {
-         arrayFilters = getReportFilterArray_MRRMarketSummary();
+        arrayFilters = getReportFilterArray_MRRMarketSummary();
     }
+
+    if (inReportId == rptXRAYUsage)
+    {
+        arrayFilters = getReportFilterArray_XRAYUsage();
+
+    }
+
+    if (inReportId == rptMRRUsage) {
+        arrayFilters = getReportFilterArray_MRRUsage();
+
+    }
+
+    if (inReportId == rptTVBUsage) {
+        arrayFilters = getReportFilterArray_TVBUsage();
+
+    }
+
+
 
     return arrayFilters;
-}
-function getReportObject(inReportId)
-{
-    var tempObject = new Object();
-
-    columnsToDisplay = new Array();
-    columnsToDisplay.push("Category");
-    columnsToDisplay.push("Current Amount");
-    columnsToDisplay.push("Previous Amount");
-    columnsToDisplay.push("Period Pct");
-    columnsToDisplay.push("Current Amount YTD");
-    columnsToDisplay.push("Previous Amount YTD");
-    columnsToDisplay.push("YTD Pct");
-
-    tempObject =
-    {
-        reportTitle: "Market Summary",
-        apiControllerAction: "/api/MRRReport/GetMarketSummary",
-        apiType: "get",
-        columnsToDisplay: columnsToDisplay
-    }
-
-    return tempObject;
 }
 
 //-------------------------
 // REPORT AREA
 //-------------------------
+
+// MRR Market Summary
 function getReportFilterArray_MRRMarketSummary()
 {
 
@@ -102,6 +115,206 @@ function getReportObject_MRRMarketSummary()
     {
         reportTitle: "Market Summary",
         apiControllerAction: "/api/MRRReport/GetMarketSummary",
+        apiType: "get",
+        columnsToDisplay: columnsToDisplay
+    }
+
+    return tempObject;
+}
+
+// XRAY Usage
+function getReportFilterArray_XRAYUsage() {
+
+    var arrayFilters = new Array();
+    var arrayObject = new Object();
+
+    arrayObject = {
+        token: "StartDate",
+        jsCall: null,
+        objectName: "dtStartDate",
+        required: true
+    }
+    arrayFilters.push(arrayObject);
+
+    arrayObject = {
+        token: "EndDate",
+        jsCall: null,
+        objectName: "dtEndDate",
+        required: true
+    }
+    arrayFilters.push(arrayObject);
+
+    arrayObject = {
+        token: "Market",
+        jsCall: "getMarketList",
+        objectName: "ddlMarket",
+        required: false
+    }
+    arrayFilters.push(arrayObject);
+
+    arrayObject = {
+        token: "Owner",
+        jsCall: "getOwnerList",
+        objectName: "ddlOwner",
+        required: false
+    }
+    arrayFilters.push(arrayObject);
+
+    return arrayFilters;
+}
+function getReportObject_XRAYUsage() {
+
+    var tempObject = new Object();
+
+    columnsToDisplay = new Array();
+    columnsToDisplay.push("QueryDateTime");
+    columnsToDisplay.push("PersonnelId");
+    columnsToDisplay.push("QueryForm");
+    columnsToDisplay.push("LoginDateTime");
+    columnsToDisplay.push("Browser");
+    columnsToDisplay.push("MarketId");
+    columnsToDisplay.push("MarketName");
+    columnsToDisplay.push("OwnerName");
+    columnsToDisplay.push("UserName");
+
+    tempObject =
+    {
+        reportTitle: "XRay Usage Report",
+        apiControllerAction: "/api/XRAYReport/GetXRayUsage",
+        apiType: "get",
+        columnsToDisplay: columnsToDisplay
+    }
+
+    return tempObject;
+}
+
+
+// MRR Usage
+function getReportFilterArray_MRRUsage() {
+
+    var arrayFilters = new Array();
+    var arrayObject = new Object();
+
+    arrayObject = {
+        token: "StartDate",
+        jsCall: null,
+        objectName: "dtStartDate",
+        required: true
+    }
+    arrayFilters.push(arrayObject);
+
+    arrayObject = {
+        token: "EndDate",
+        jsCall: null,
+        objectName: "dtEndDate",
+        required: true
+    }
+    arrayFilters.push(arrayObject);
+
+    arrayObject = {
+        token: "Market",
+        jsCall: "getMarketList",
+        objectName: "ddlMarket",
+        required: false
+    }
+    arrayFilters.push(arrayObject);
+
+    arrayObject = {
+        token: "Owner",
+        jsCall: "getOwnerList",
+        objectName: "ddlOwner",
+        required: false
+    }
+    arrayFilters.push(arrayObject);
+
+    return arrayFilters;
+}
+function getReportObject_MRRUsage() {
+
+    var tempObject = new Object();
+
+    columnsToDisplay = new Array();
+    columnsToDisplay.push("QueryDateTime");
+    columnsToDisplay.push("PersonnelId");
+    columnsToDisplay.push("ReportTitle");
+    columnsToDisplay.push("LoginDateTime");
+    columnsToDisplay.push("Browser");
+    columnsToDisplay.push("MarketId");
+    columnsToDisplay.push("MarketName");
+    columnsToDisplay.push("OwnerGroupName");
+    columnsToDisplay.push("UserName");
+
+    tempObject =
+    {
+        reportTitle: "MRR Usage Report",
+        apiControllerAction: "/api/MRRReport/GetMRRUsage",
+        apiType: "get",
+        columnsToDisplay: columnsToDisplay
+    }
+
+    return tempObject;
+}
+
+
+// TVB Usage
+function getReportFilterArray_TVBUsage() {
+
+    var arrayFilters = new Array();
+    var arrayObject = new Object();
+
+    arrayObject = {
+        token: "StartDate",
+        jsCall: null,
+        objectName: "dtStartDate",
+        required: true
+    }
+    arrayFilters.push(arrayObject);
+
+    arrayObject = {
+        token: "EndDate",
+        jsCall: null,
+        objectName: "dtEndDate",
+        required: true
+    }
+    arrayFilters.push(arrayObject);
+
+    arrayObject = {
+        token: "Market",
+        jsCall: "getMarketList",
+        objectName: "ddlMarket",
+        required: false
+    }
+    arrayFilters.push(arrayObject);
+
+    arrayObject = {
+        token: "Owner",
+        jsCall: "getOwnerList",
+        objectName: "ddlOwner",
+        required: false
+    }
+    arrayFilters.push(arrayObject);
+
+    return arrayFilters;
+}
+function getReportObject_TVBUsage() {
+
+    var tempObject = new Object();
+
+    columnsToDisplay = new Array();
+    columnsToDisplay.push("ReportDateTime");
+    columnsToDisplay.push("PersonnelId");
+    columnsToDisplay.push("ReportTitle");
+    columnsToDisplay.push("LoginDateTime");
+    columnsToDisplay.push("Browser");
+    columnsToDisplay.push("MarketNames");
+ 
+    columnsToDisplay.push("OwnerNames");
+    columnsToDisplay.push("UserName");
+
+    tempObject =
+    {
+        reportTitle: "TVB Usage Report",
+        apiControllerAction: "/api/TVBReport/GetTVBUsage",
         apiType: "get",
         columnsToDisplay: columnsToDisplay
     }
