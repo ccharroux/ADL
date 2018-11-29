92c39231f86dd1bb810e82edc11f6e4ad18f9210
MKACPA\ccharroux <ccharroux@millerkaplan.com>
2018-11-29 13:59:29 -0800

    config changes to help with cert issues on dev and staging...

--- InternalWebApp/InternalWebApp/js/config.js
+++ InternalWebApp/InternalWebApp/js/config.js
@@ -1,15 +1,30 @@
 var gTimeToExpire = 6000; // seconds
 var ServicePrefix = "https://devservices.millerkaplan.com";
 
-
+var bNonProd = true;
 if (window.location.toString().toLowerCase().indexOf("devmediainternal.millerkaplan.com") > -1) {
     ServicePrefix = "https://devservices.millerkaplan.com/";
+    bNonProd = true;
 }
 else if (window.location.toString().toLowerCase().indexOf("stagingmediainternal.millerkaplan.com") > -1) {
     ServicePrefix = "https://stagingservices.millerkaplan.com/";
+    bNonProd = true;
 }
 else if (window.location.toString().toLowerCase().indexOf("mediainternal.millerkaplan.com") > -1) {
     ServicePrefix = "https://services.millerkaplan.com/";
+    bNonProd = false;
+}
+
+var currentPage = window.location.toString().toLocaleLowerCase();
+ 
+    if (currentPage.indexOf("http:") > -1)
+{
+    // not secure...
+    // and not prod
+    if (bNonProd)
+    {
+          ServicePrefix = ServicePrefix.replace("https:", "http:");
+    }
 }
 
 
