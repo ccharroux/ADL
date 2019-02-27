8c754a2562c28e8eece6a97ce3fee1a8d406d48f
MKACPA\ccharroux <ccharroux@millerkaplan.com>
2019-02-27 07:45:40 -0800

    xry release

--- InternalWebApp/InternalWebApp/js/main.js
+++ InternalWebApp/InternalWebApp/js/main.js
@@ -610,6 +610,7 @@ function buildXRYMenu(selectedItem) {
     menuItems += '<nav role="navigation" style="margin-top:20px">'
 
     menuItems += '<ul>';
+    menuItems += '        <li><a ' + getSelectedItemClass(selectedItem, "Release") + 'href="releasexry.html">Release</a></li>';
     menuItems += buildGenericReportsLink(selectedItem);
     menuItems += '       <li class="dropdown"><a ' + getSelectedItemClass(selectedItem, "Products") + ' href="" role="button" aria-expanded="false">Products <span style="margin-right:10px;" class="caret"></span></a>';
     menuItems += '              <ul class="dropdown-menu" role="menu">';
