2c4d4ec7ccdaaffb2794d932e61a4a64fb5bd58f
Brian Jenkins <bjenkins@millerkaplan.com>
2020-08-31 15:46:16 -0700

    DEV-7913 added message for when uploading media/third party data.

--- InternalWebApp/InternalWebApp/js/main.js
+++ InternalWebApp/InternalWebApp/js/main.js
@@ -3015,3 +3015,53 @@ function widerReport()
  
  
 }
+
+function convertMontNumberToText(inMonth)
+{
+    var sInMonthText = String(inMonth);
+
+    var monthText = "";
+
+    switch(sInMonthText)
+    {
+        case "1":
+            monthText = "January";
+            break;
+        case "2":
+            monthText = "February";
+            break;
+        case "3":
+            monthText = "March";
+            break;
+        case "4":
+            monthText = "April";
+            break;
+        case "5":
+            monthText = "May";
+            break;
+        case "6":
+            monthText = "June";
+            break;
+        case "7":
+            monthText = "July";
+            break;
+        case "8":
+            monthText = "August";
+            break;
+        case "9":
+            monthText = "September";
+            break;
+        case "10":
+            monthText = "October";
+            break;
+        case "11":
+            monthText = "November";
+            break;
+        case "12":
+            monthText = "December";
+            break;
+        
+    }
+
+    return monthText;
+}
