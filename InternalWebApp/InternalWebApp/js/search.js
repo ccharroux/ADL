3759c0066129c7cb6b712c6d193d477a8a9d8a83
Brian Jenkins <bjenkins@millerkaplan.com>
2020-09-04 10:20:42 -0700

    DEV-7907 fixed field sizing on the advertiser and agency CRUD pages. Also some search issues that i found.

--- InternalWebApp/InternalWebApp/js/search.js
+++ InternalWebApp/InternalWebApp/js/search.js
@@ -825,7 +825,20 @@ function buildParentAgencySearch(searchCriteria) {
     if ($('.search-text:visible').val().length > 0) {
         searchText = $('.search-text:visible').val();
     } else {
-        searchText = searchCriteria["linkParentAgencyName"].length > 0 ? searchCriteria["linkParentAgencyName"] : searchCriteria["agencyName"];
+        if (searchCriteria["linkParentAgencyName"].length > 0)
+        {
+            searchText = searchCriteria["linkParentAgencyName"].substring(0, gMinimumSearchCharacters);
+        } else if (searchCriteria["agencyName"] != undefined)
+        {
+            searchText = searchCriteria["agencyName"].substring(0, gMinimumSearchCharacters);
+        } else if (searchCriteria["parentAgencyName"] != undefined) {
+            searchText = searchCriteria["parentAgencyName"].substring(0, gMinimumSearchCharacters);
+        }
+        //searchText = searchCriteria["linkParentAgencyName"].length > 0 ? searchCriteria["linkParentAgencyName"].substring(0, gMinimumSearchCharacters) : searchCriteria["agencyName"].substring(0, gMinimumSearchCharacters);
+    }
+
+    if ($('.search-text:visible').val().length == 0 && gSilentSearch == true) {
+        $('.search-text:visible').val(searchText);
     }
 
     apiParameters = {
@@ -1007,7 +1020,11 @@ function buildLinkAdvertiserSearch(searchCriteria) {
     if ($('.search-text:visible').val().length > 0) {
         searchText = $('.search-text:visible').val();
     } else {
-        searchText = searchCriteria["linkedAdvertiserName"].length > 0 ? searchCriteria["linkedAdvertiserName"] : searchCriteria["advertiserName"];
+        searchText = searchCriteria["linkedAdvertiserName"].length > 0 ? searchCriteria["linkedAdvertiserName"].substring(0, gMinimumSearchCharacters) : searchCriteria["advertiserName"].substring(0, gMinimumSearchCharacters);
+    }
+
+    if ($('.search-text:visible').val().length == 0 && gSilentSearch == true) {
+        $('.search-text:visible').val(searchText);
     }
 
     //setup the parameters for the API
