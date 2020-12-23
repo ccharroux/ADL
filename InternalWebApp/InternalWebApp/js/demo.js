85ff60d24fcd29a3f0fbba16623d319bcb8e75a3
Edward Murphy <emurphy@mkasystems.com>
2020-12-23 09:55:56 -0800

    DEV-8824 DEV-8825 DEV-8826 DEV-9223 limit demo product list to MRR XRY new demo market: filter market list by product list markets consistently as "Name (ID)"

---
+++ InternalWebApp/InternalWebApp/js/demo.js
@@ -0,0 +1,13 @@
+﻿// DEV-8824
+function getDemoProductList() {
+    return [
+        {
+            "demoProductId": "MRR",
+            "demoProductName": "Market Revenue"
+        },
+        {
+            "demoProductId": "XRY",
+            "demoProductName": "X-Ray"
+        }
+    ];
+}
