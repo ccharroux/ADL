0984dfca76ed739ab6a3871413148eb277893571
Carl Charroux <carlcharroux@yahoo.com>
2022-05-05 18:28:55 -0700

    address controller

---
+++ Source/repos/ADL-API-Core/Controllers/Address/AddressController.cs
@@ -0,0 +1,107 @@
+﻿using ADLAPICore.Library.Base;
+using ADLAPICore.Library.Address;
+using ADLAPICore.Library.utilities;
+using ADLAPICore.Models.Address;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using System;
+using System.Net.Mime;
+
+namespace ADLAPICore.Controllers.Address
+{
+
+    [ApiController]
+    [Route("[controller]")]
+ 
+    public class AddressController : ControllerBase
+    {
+        private readonly ILogger<AddressController> _logger;
+        private readonly IAddressClass _controllerClass;
+
+        public AddressController(ILogger<AddressController> logger,
+                                    IAddressClass controllerClass)
+        {
+            _logger = logger;
+            _controllerClass = controllerClass;
+        }
+
+        [HttpGet()]
+        public ActionResult Get([FromQuery] AddressGetInput input)
+        {
+            try
+            {
+
+                //----------------------------------
+                // Clean inputs using reflection
+                //----------------------------------
+                var cleanInput = (AddressGetInput)genericLogic.cleanAllTextFromObject(input);
+
+                var result = _controllerClass.GetAddress(cleanInput);
+
+                return Content(JsonConvert.SerializeObject(result), MediaTypeNames.Application.Json);
+            }
+
+            catch (Exception ex)
+            {
+
+                AddressDetailRow r = new AddressDetailRow();
+                r.response = General.buildError(ex.Message);
+
+                return Content(JsonConvert.SerializeObject(r), MediaTypeNames.Application.Json);
+            }
+        }
+
+        [HttpPut()]
+        public ActionResult Put([FromBody] AddressUpdateInput input)
+        {
+            try
+            {
+
+                //----------------------------------
+                // Clean inputs using reflection
+                //----------------------------------
+                var cleanInput = (AddressUpdateInput)genericLogic.cleanAllTextFromObject(input);
+
+                var result = _controllerClass.UpdateAddress(cleanInput);
+
+                return Content(JsonConvert.SerializeObject(result), MediaTypeNames.Application.Json);
+            }
+
+            catch (Exception ex)
+            {
+
+                AddressUpdateResult r = new AddressUpdateResult();
+                r.response = General.buildError(ex.Message);
+
+                return Content(JsonConvert.SerializeObject(r), MediaTypeNames.Application.Json);
+            }
+        }
+
+        [HttpPost()]
+        public ActionResult Post([FromBody] AddressInsertInput input)
+        {
+            try
+            {
+
+                //----------------------------------
+                // Clean inputs using reflection
+                //----------------------------------
+                var cleanInput = (AddressInsertInput)genericLogic.cleanAllTextFromObject(input);
+
+                var result = _controllerClass.InsertAddress(cleanInput);
+
+                return Content(JsonConvert.SerializeObject(result), MediaTypeNames.Application.Json);
+            }
+
+            catch (Exception ex)
+            {
+
+                AddressInsertResult r = new AddressInsertResult();
+                r.response = General.buildError(ex.Message);
+
+                return Content(JsonConvert.SerializeObject(r), MediaTypeNames.Application.Json);
+            }
+        }
+    }
+}
