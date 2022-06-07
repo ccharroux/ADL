81b14bee6fa30239b42546c7afac3a7416dc5bbe
Carl Charroux <carlcharroux@yahoo.com>
2022-06-06 18:54:00 -0700

    more updates

---
+++ Source/repos/ADL-API-Core/Models/ADL/ADLPostInput.cs
@@ -0,0 +1,9 @@
+﻿namespace ADLAPICore.Models.ADL
+{
+    public class ADLInsertInput
+    {
+        public string inApiToken { get; set; }
+        public string inSystemADL { get; set; }
+        public int inSystemADLTypeId { get; set; }
+    }
+}
---
+++ Source/repos/ADL-API-Core/Models/ADL/ADLPutInput.cs
@@ -0,0 +1,16 @@
+﻿namespace ADLAPICore.Models.ADL
+{
+    public class ADLDeleteInput
+    {
+        public string inApiToken { get; set; }
+        public int inSystemADLId { get; set; }
+    }
+    public class ADLUpdateInput
+    {
+        public string inApiToken { get; set; }
+        public int inSystemADLId { get; set; }
+        public string inSystemADL { get; set; }
+        public int inSystemADLTypeId { get; set; }
+        public int inOrderNum { get; set; }
+    }
+}
