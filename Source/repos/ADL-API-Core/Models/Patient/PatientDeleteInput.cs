99d311e1a417547b7a1697e7540c1de1cc002b55
Carl Charroux <carlcharroux@yahoo.com>
2022-06-27 18:53:18 -0700

    update patient adl

---
+++ Source/repos/ADL-API-Core/Models/Patient/PatientDeleteInput.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ADLAPICore.Models.Patient
+{
+
+
+
+    public class PatientADLDeleteInput
+    {
+        public string inApiToken { get; set; }
+        public int inPatientId { get; set; }
+        public int inSystemADLId { get; set; }
+    }
+
+
+}
