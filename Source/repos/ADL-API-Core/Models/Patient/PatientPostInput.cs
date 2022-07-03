3edcdcb54149149008829627ae76facae08f0152
Carl Charroux <carlcharroux@yahoo.com>
2022-07-03 12:46:47 -0700

    dashboard coming along

---
+++ Source/repos/ADL-API-Core/Models/Patient/PatientPostInput.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ADLAPICore.Models.Patient
+{
+
+    public class PatientADLLogInsertInput
+    {
+        public string inApiToken { get; set; }        
+        public int inUserId { get; set; }
+        public int inADLPatientId { get; set; }
+ 
+ 
+    }
+
+ 
+
+}
---
+++ Source/repos/ADL-API-Core/Models/Patient/PatientUpdateInput.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ADLAPICore.Models.Patient
+{
+
+    public class PatientADLUpdateInput
+    {
+        public string inApiToken { get; set; }        
+        public int inPatientId { get; set; }
+        public int inSystemADLId { get; set; }
+        //public List<PatientADLUpdateRow> rows = new List<PatientADLUpdateRow>();
+    }
+
+    public class PatientADLUpdateRow
+    {
+        public int inDayOfTheWeek { get; set; }
+        public string inTimeOfDay { get; set; }
+        public bool inDelete { get; set; }
+    }
+    
+    public class PatientADLUpdateDBRow
+    {
+        public string inApiToken { get; set; }
+        public int inPatientId { get; set; }
+        public int inSystemADLId { get; set; }
+        public int inDayOfTheWeek { get; set; }
+        public string inTimeOfDay { get; set; }
+        public int inDelete { get; set; }
+    }
+
+}
