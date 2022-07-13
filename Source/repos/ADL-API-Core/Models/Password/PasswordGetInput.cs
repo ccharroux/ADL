f61bcf78b2edf22ac5e03df52dd79f6bd83cf87e
Carl Charroux <carlcharroux@yahoo.com>
2022-07-13 16:45:55 -0700

    password updates

---
+++ Source/repos/ADL-API-Core/Models/Password/PasswordGetInput.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace ADLAPICore.Models.Password
+{
+    public class PasswordResetTokenGetInput
+    {
+        public string inPasswordResetToken { get; set; }
+    }
+
+
+}
